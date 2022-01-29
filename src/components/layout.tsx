a5ea61ce11ffd260574638d590dd25f05618d48e
Pirmin Bahr <bahr.pirmin@web.de>
2022-01-29 10:21:41 +0100

    fix(home): grid to tailwind

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,11 +4,11 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-stone-200">
-      <div className="container p-4 mx-auto min-h-screen relative max-w-full">
+    <div className="bg-stone-100">
+      <div className="container p-4 mx-auto min-h-screen relative">
         <nav className="flex justify-between items-center relative">
           <h1 className="text-4xl font-bold border-2 border-black px-2 rounded my-2">PPR</h1>
-          <div className="bg-zinc-300 rounded-3xl fixed mx-auto w-3/12 left-0 right-0">
+          <div className="bg-zinc-200 rounded-3xl fixed mx-auto w-3/12 left-0 right-0">
             <ul className="p-2 z-1 m-0 flex cursor-pointer w-full justify-between">
               <li className="rounded-2xl group hover:bg-white py-1 px-4 bg-white"><a className="group-hover:text-gray-700">Home</a></li>
               <li className="rounded-2xl group hover:bg-white py-1 px-4"><a className="group-hover:text-gray-700">Projects</a></li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,7 @@ import Layout from "../components/layout";
 export default function Home() {
   return (
     <Layout>
-      <div style={{
-        padding: "40px 0 80px",
-        maxWidth: "1200px",
-        margin: "0 auto",
-        display: "grid",
-        gridTemplateColumns: "repeat(4,1fr)",
-        gridTemplateRows: "280px 280px 280px 280px",
-        gridGap: "1rem"
-      }}>
+      <div className="mansory" style={{ gridTemplateRows: "280px 280px 280px 280px" }}>
         <div className="box box-2w"></div>
         <div className="box"></div>
         <div className="box box-2h"></div>
